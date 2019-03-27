7ef19d3f6b64d274d3ebdc681348207d90f2d9bc
EvgeniKonstantinov <nenyznoe.mbllo@gmail.com>
2019-03-27 23:11:24 +0300

    create header

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,14 @@ import "../App.scss";
 const Body = () => {
   return (
     <section className="body">
-      <p>Test body</p>
+      <div className="left_block blue">
+        <div>
+          <h1>Travel Agents Portal</h1>
+        </div>
+      </div>
+      <div className="right_block">
+        <img src="" alt="" />
+      </div>
     </section>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
+import { ReactComponent as Logo } from "../img/logo.svg";
+import { ReactComponent as Cart } from "../img/cart.svg";
+import { ReactComponent as Account } from "../img/account.svg";
+import { ReactComponent as Call } from "../img/call-icon.svg";
 import "../App.scss";
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="left_blockl">
-        <img src="" alt="Logo Img" />
+      <div className="left_block">
+        <div className="logo_block">
+          <Logo />
+        </div>
       </div>
       <div className="right_block">
         <nav>
           <ul>
-            <li>Page1</li>
-            <li>Page2</li>
-            <li>Page3</li>
+            <li>DESTINATIONS</li>
+            <li>Style of Travel</li>
+            <li>About Us</li>
           </ul>
         </nav>
+        <div className="button_block">
+          <div className="button">
+            <Call />
+          </div>
+          <div className="button">
+            <Account />
+          </div>
+          <div className="button blue">
+            <Cart />
+          </div>
+        </div>
       </div>
     </header>
   );
